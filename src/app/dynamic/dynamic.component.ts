231d1f06d37477ab1286a14469a949fdd7027ef2
Marko Berger <marko.berger@intellegens.hr>
2019-08-30 10:06:23 +0200

    comments

--- src/app/dynamic/dynamic.component.ts
+++ src/app/dynamic/dynamic.component.ts
@@ -3,7 +3,7 @@ import { Component1Component } from '../component1/component1.component';
 import { PlaceholderDirective } from '../placeholder.directive';
 import { Component2Component } from '../component2/component2.component';
 import { IComp } from '../icomp';
-import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-dynamic',
@@ -12,38 +12,44 @@ import { Observable } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DynamicComponent implements OnInit, OnChanges{
+  /**Here we grab reference placeholder directive  */
   @ViewChild(PlaceholderDirective, {static: true}) placeholder: PlaceholderDirective;
-  @Input() inputData: number; 
-  @Output() changeEmit: EventEmitter<string> = new EventEmitter<string>(); 
+  @Input() inputData: number;
+  @Output() changeEmit: EventEmitter<string> = new EventEmitter<string>();
+  /**An array where we register what component we want to load */
   components = [Component1Component, Component2Component];
 
   constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
 
   ngOnInit() {
-    console.log('onInit', this.inputData);
-    
-    // this.loadComponent();
   }
   ngOnChanges(changes: SimpleChanges): void {
-    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
-    //Add '${implements OnChanges}' to the class.
-    console.log('sC', changes);
-    
-    if(changes.inputData.currentValue != undefined){
+    /**
+     * We are using this hook for change detection and invoking loadComponent() method
+     * There are more ways to do this but for the simplicity of this example I have decided on this way
+     */
+    if(changes.inputData.currentValue != undefined){ // We need to check if inputData has some value
       this.loadComponent();
     }
   }
 
-  
+  /**
+   * A method that loads and creates instances of components
+   */
   loadComponent(){
-    console.log('call');
-    
+    /** Preparing our component for creation */
     const componentFactory = this.componentFactoryResolver.resolveComponentFactory(this.components[this.inputData]);
+    /** Grabbing reference of our view placeholder */
     const viewContainerRef = this.placeholder.viewContainerRef;
-
+    /** Clearing our placeholder  */
     viewContainerRef.clear();
+    /** Magic of creating a component instance  */
     const componentRef = viewContainerRef.createComponent(componentFactory);
+    /** 
+     * @Input data into our instance.
+     */
     (componentRef.instance as IComp).text = ''+this.inputData;
+    /** @Output data from our instance  */
     (componentRef.instance as IComp).event.subscribe(
       data => this.changeEmit.emit(data)
     );
